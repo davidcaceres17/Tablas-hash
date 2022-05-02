07ab6d7e6ec8d77b48e913ec7d637ae1b0c4f8e5
davidcaceres17 <50807080+davidcaceres17@users.noreply.github.com>
2022-05-01 23:24:02 -0600

    Add files via upload
    Subiendo el proyecto terminado, ingeniero espero cumplir con lo pedido realice las pruebas gracias

---
+++ Tabla_hash_justo_caceres/src/Clases/clsCodigos.java
@@ -0,0 +1,19 @@
+package Clases;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author willg
+ */
+public class clsCodigos {
+    public String CodigoEx1 = "int suma = 0;"
+            + "suma = 54 + 20;";
+    
+    public String CodigoEx2 = "int suma = 0;"
+            + "suma = 54 + 20;";
+}
